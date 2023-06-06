59732cbe3fed944915d9fb364f829567524931fa
Favour Akpan <favourgabriel50@gmail.com>
2023-06-06 13:25:03 +0100

    working on the create booking functionality

--- app/Repository/Eloquent/BookingRepository.php
+++ app/Repository/Eloquent/BookingRepository.php
@@ -4,26 +4,33 @@ namespace App\Repository\Eloquent;
 
 use App\Repository\BaseRepository;
 use App\Models\User;
+use App\Models\Booking;
 
 class BookingRepository extends BaseRepository
 {
     protected $user;
+    protected $book;
 
-    public function __construct( User $user){
+    public function __construct( User $user, Booking $book){
         $this->user = $user;
+        $this->book = $book;
+    }
+
+    public function thisUser(){
+       return  $this->user->where('email', auth()->user()->email)->first();
     }
 
     public function create($data){
 
-
-        if(auth()->user() !== $this->user ){
+        
+        if(auth()->user() !== $this->user){
             return [
                 "status" => $this->failResponse('Unauthorised'),
             ];
         }
 
         return[
-            'status' => $this->successResponse('book has been created successfully'),
+            'status' => $this->isSuccessful(),
             'data' => $this->book->create($data->all()),
         ];
 
